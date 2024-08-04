da13027e3b4bee084f695441e592802d2fd07624
umutcan <eubalcin@gmail.com>
2024-08-04 14:48:28 +0300

    etre

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,9 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  url: env('RENDER_EXTERNAL_URL', 'https://strapi-cms-odlu.onrender.com'),
 });
