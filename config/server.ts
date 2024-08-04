47b408ac7fea7f15848aba9834a4a36fdf639e2c
umutcan <eubalcin@gmail.com>
2024-08-04 14:36:27 +0300

    externalurl

--- config/server.ts
+++ config/server.ts
@@ -7,4 +7,5 @@ export default ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  url: env('RENDER_EXTERNAL_URL', 'https://strapi-cms-odlu.onrender.com'),
 });
