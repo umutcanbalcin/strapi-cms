c717a3ce2316304dfe97e79730f03e75b20d01b3
umutcan <eubalcin@gmail.com>
2024-08-04 15:27:36 +0300

    server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array('APP_KEYS')
   },
 });
