c47b80dff3e7c026c21f9ff8e09aa579c38544b8
umutcan <eubalcin@gmail.com>
2024-08-04 15:06:43 +0300

    son

--- config/server.ts
+++ config/server.ts
@@ -1,9 +1,10 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
